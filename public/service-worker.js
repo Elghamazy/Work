82418727c8a3cc71d2d08cc054e9b8393cfa4ecd
Elghamazy <130920096+Elghamazy@users.noreply.github.com>
2024-08-20 18:22:04 +0000

    bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,47 @@
-self.addEventListener('install', (event) => {
+const PRECACHE = 'precache-v3';
+const RUNTIME = 'runtime';
+
+const PRECACHE_URLS = [
+  './',
+  'styles.css',
+  'offline.js',
+  'view_shifts.html',
+];
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('shift-tracker-cache-v2').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/offline.html'
-      ]);
-    })
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
   );
+});
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
